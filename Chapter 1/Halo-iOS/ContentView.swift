f2eb6b272414e9974b3424c629ec81cf0e496aa1
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-11-21 09:40:10 -0800

    Added Chapter 1

---
+++ Chapter 1/Halo-iOS/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Halo-iOS
+//
+//  Created by Cyril Zakka on 10/21/24.
+//
+
+import SwiftUI
+import AccessorySetupKit
+
+struct ContentView: View {
+    
+    @State var ringSessionManager = RingSessionManager()
+    
+    var body: some View {
+        List {
+            Section("MY DEVICE", content: {
+                if ringSessionManager.pickerDismissed, let currentRing = ringSessionManager.currentRing {
+                    makeRingView(ring: currentRing)
+                } else {
+                    Button {
+                        ringSessionManager.presentPicker()
+                    } label: {
+                        Text("Add Ring")
+                            .frame(maxWidth: .infinity)
+                            .font(Font.headline.weight(.semibold))
+                    }
+                }
+            })
+        }.listStyle(.insetGrouped)
+        
+    }
+    
+    @ViewBuilder
+    private func makeRingView(ring: ASAccessory) -> some View {
+        HStack {
+            Image("colmi")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 70)
+            
+            VStack(alignment: .leading) {
+                Text(ring.displayName)
+                    .font(Font.headline.weight(.semibold))
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
