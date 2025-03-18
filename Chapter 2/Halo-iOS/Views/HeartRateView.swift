1e2bb1de36e3e835e71330377dbeae8b1bec0af9
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2025-03-17 17:32:55 -0700

    Prep for battery/heart/spo2

---
+++ Chapter 2/Halo-iOS/Views/HeartRateView.swift
@@ -0,0 +1,18 @@
+//
+//  HeartRateView.swift
+//  Halo-iOS
+//
+//  Created by Cyril Zakka on 3/17/25.
+//
+
+import SwiftUI
+
+struct HeartRateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeartRateView()
+}
